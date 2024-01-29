6aec7fafe0698dabbee13ec499cb51a045d644ed
yassineboucham <yassineboucham11@gamil.com>
2024-01-29 13:28:22 +0000

    all

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -13,9 +13,9 @@ void selection_sort(int *array, size_t size)
 	if (!array || !size)
 		return;
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size - 1; i++)
 	{
-		for (j = i; j < size; j++)
+		for (j = i + 1; j < size; j++)
 		{
 			if (array[i] > array[j])
 			{
