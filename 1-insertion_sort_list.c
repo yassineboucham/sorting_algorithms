f726fd2f0dce298b2292f9f785be3daa69771ec6
yassineboucham <yassineboucham11@gamil.com>
2024-01-29 12:50:05 +0000

    all

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include "sort.h"
+/**
+* swap - swapping the valuse
+* @a: value 1
+* @b: value 2
+*/
+void swap(listint_t *a, listint_t *b)
+{
+	if (a->prev)
+		a->prev->next = b;
+	if (b->next)
+		b->next->prev = a;
+	a->next = b->next;
+	b->prev = a->prev;
+	a->prev = b;
+	b->next = a;
+}
+/**
+ * insertion_sort_list - insertion sort list
+ * @list: double linked list
+*/
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *i, *j;
+
+	if (!list || !*list || !(*list)->next)
+		return;
+	i = (*list)->next;
+	while (i)
+	{
+		j = i;
+		i = i->next;
+		while (j && j->prev)
+			if (j->prev->n > j->n)
+			{
+				swap(j->prev, j);
+				if (!j->prev)
+					*list = j;
+				print_list((const listint_t *)*list);
+			}
+			else
+				j = j->prev;
+	}
+}
